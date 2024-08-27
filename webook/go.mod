7704716e84d539704eddcd9de2b530ac863a5db2
Ting <Ting@forwork.com>
2024-08-27 23:35:28 +0800

    feat: SMS login

--- webook/go.mod
+++ webook/go.mod
@@ -6,6 +6,7 @@ toolchain go1.23.0
 
 require (
 	github.com/dlclark/regexp2 v1.11.4
+	github.com/ecodeclub/ekit v0.0.9
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-contrib/sessions v1.0.1
 	github.com/gin-gonic/gin v1.10.0
@@ -14,6 +15,7 @@ require (
 	github.com/gorilla/sessions v1.4.0
 	github.com/pelletier/go-toml/v2 v2.2.2
 	github.com/redis/go-redis/v9 v9.6.1
+	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.991
 	golang.org/x/crypto v0.26.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.11
@@ -45,6 +47,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/quasoft/memstore v0.0.0-20191010062613-2bce066d2b0b // indirect
+	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.991 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
---
+++ webook/internal/service/sms/tencent/service.go
@@ -0,0 +1,49 @@
+package tencent
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ecodeclub/ekit"
+	"github.com/ecodeclub/ekit/slice"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+)
+
+type Service struct {
+	appId     *string // 取指针 是因为腾讯云SMS的设计要求
+	signature *string
+	client    *sms.Client
+}
+
+func NewService(client *sms.Client, appId string, signature string) *Service {
+	return &Service{
+		client:    client,
+		appId:     ekit.ToPtr[string](appId),
+		signature: ekit.ToPtr[string](signature),
+	}
+}
+
+func (s *Service) Send(c context.Context, tplId string, args []string, numbers ...string) error {
+	req := sms.NewSendSmsRequest()
+	req.SmsSdkAppId = s.appId
+	req.SignName = s.signature
+	req.TemplateId = ekit.ToPtr[string](tplId)
+	req.PhoneNumberSet = s.toStringPtrSlice(numbers)
+	req.TemplateParamSet = s.toStringPtrSlice(args)
+	resp, err := s.client.SendSms(req)
+	if err != nil {
+		return fmt.Errorf("error.%w\n", err)
+	}
+	for _, status := range resp.Response.SendStatusSet {
+		if status.Code == nil || *(status.Code) == "Ok" {
+			return fmt.Errorf("send message failed, code:%s, message:%s", *status.Code, *status.Message)
+		}
+	}
+	return nil
+}
+
+func (s *Service) toStringPtrSlice(src []string) []*string {
+	return slice.Map[string, *string](src, func(idx int, src string) *string {
+		return &src
+	})
+}
