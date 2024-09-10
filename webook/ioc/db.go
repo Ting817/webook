6576559c30a93ea702c98bd44ee8bfc31a9d8335
Ting <Ting@forwork.com>
2024-09-10 21:13:02 +0800

    feat(log): 利用gorm打印日志

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -3,13 +3,25 @@ package ioc
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	glogger "gorm.io/gorm/logger"
+	"time"
 	"webook/pkg/cfg"
+	"webook/pkg/logger"
 
 	"webook/internal/repository/dao"
 )
 
-func InitDB(c cfg.Config) *gorm.DB {
-	db, err := gorm.Open(mysql.Open(c.DB.DSN), &gorm.Config{})
+func InitDB(c cfg.Config, l logger.LoggerV1) *gorm.DB {
+	db, err := gorm.Open(mysql.Open(c.DB.DSN), &gorm.Config{
+		Logger: glogger.New(gormLoggerFunc(l.Debug), glogger.Config{
+			// 慢查询阈值，只有执行时间超过这个阈值才会使用
+			// SQL 查询必然要求命中索引，最好走一次磁盘 IO
+			// 一次磁盘 IO 是不到 10ms
+			SlowThreshold:             time.Millisecond * 10,
+			IgnoreRecordNotFoundError: true,
+			LogLevel:                  glogger.Info,
+		}),
+	})
 	if err != nil {
 		panic(err) // panic 即goroutine直接结束 只在初始化时考虑panic
 	}
@@ -18,4 +30,14 @@ func InitDB(c cfg.Config) *gorm.DB {
 	}
 
 	return db
+}
+
+// 单接口可以这样用
+type gormLoggerFunc func(msg string, fields ...logger.Field)
+
+func (g gormLoggerFunc) Printf(msg string, args ...interface{}) {
+	g(msg, logger.Field{
+		Key:   "args",
+		Value: args,
+	})
 }
